30d7c0d250c7672d00e54e0f45c1348ef7433cc0
Jean <jeancarlos.av0@gmail.com>
2023-11-23 18:20:19 -0300

    Adicionando o arquivo Inventario.php juntamente com as documentações iniciais para classes e métodos

---
+++ Inventario.php
@@ -0,0 +1,15 @@
+<?php
+
+    require_once('Item.php');
+
+    /**
+    * @method int getCapacidadeMaxima() 
+    * @method void setCapacidadeMaxima(int $capacidadeMaxima) 
+    * @method array getItens() 
+    * @method bool adicionarItem(Item $item) 
+    * @method bool removerItem(Item $item) 
+    * @method int capacidadeLivre() 
+    */
+    
+
+?>
