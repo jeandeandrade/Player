c7b716f174e569e4c6ccfac6531df31d5fc1c462
Jean <jeancarlos.av0@gmail.com>
2023-11-23 10:24:37 -0300

    Documentações Iniciais para classes e métodos

---
+++ Player.php
@@ -0,0 +1,16 @@
+<?php
+
+    /**
+     * @method string getNickname()
+     * @method int getNivel()
+     * @method Inventario getInventario()
+     * @method void setNickname(string $nickname)
+     * @method void setNivel(int $nivel)
+     * @method void setInventario(Inventario $inventario)
+     * @method void coletarItem(Item $item)
+     * @method void soltarItem(Item $item)
+     * @method void subirNivel()
+    */
+    
+
+?>
