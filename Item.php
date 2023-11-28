29da4828727686dc17b86d11238928855e7e0ce1
Jean <jeancarlos.av0@gmail.com>
2023-11-28 10:18:26 -0300

    Adicionado o Constructor e os métodos getters e setters

--- Item.php
+++ Item.php
@@ -16,6 +16,74 @@
          * @var string Classe do item
          */
         protected $classe;
+
+        /**
+         * Constructor da classe Item
+         * 
+         * @param string $name
+         * @param int $tamanho
+         * @param string $classe
+         */
+        public function __construct($name, $tamanho, $classe)
+        {
+            $this->name = $name;
+            $this->tamanho = $tamanho;
+            $this->classe = $classe;
+        }
+
+        /**
+         * Obtém o nome do item.
+         *
+         * @return string O nome do item.
+         */
+        public function getName() {
+            return $this->name;
+        }
+
+        /**
+         * Define o nome do item.
+         *
+         * @param string $name O nome do item.
+         */
+        public function setName($name) {
+            $this->name = $name;
+        }
+
+        /**
+         * Obtém o tamanho do item.
+         *
+         * @return int O tamanho do item.
+         */
+        public function getTamanho() {
+            return $this->tamanho;
+        }
+
+        /**
+         * Define o tamanho do item.
+         *
+         * @param int $tamanho O tamanho do item.
+         */
+        public function setTamanho($tamanho) {
+            $this->tamanho = $tamanho;
+        }
+
+        /**
+         * Obtém a classe do item.
+         *
+         * @return string A classe do item.
+         */
+        public function getClasse() {
+            return $this->classe;
+        }
+
+        /**
+         * Define a classe do item.
+         *
+         * @param string $classe A classe do item.
+         */
+        public function setClasse($classe) {
+            $this->classe = $classe;
+        }
     }
 
 ?>
