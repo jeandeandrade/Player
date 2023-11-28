8378222405eff5e78f6c473810b11dd34a4537b8
Jean <jeancarlos.av0@gmail.com>
2023-11-28 10:09:02 -0300

    Iniciando o arquivo Item

---
+++ Item.php
@@ -0,0 +1,21 @@
+<?php
+
+    class Item
+    {
+        /**
+         * @var string Nome do item
+         */
+        protected $name;
+
+        /**
+         * @var int Tamanho do item
+         */
+        protected $tamanho;
+
+        /**
+         * @var string Classe do item
+         */
+        protected $classe;
+    }
+
+?>
